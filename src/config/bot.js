d0b838ce0a88eeb1560d94818c182577d0860642
jmwff <131554309+jmwff@users.noreply.github.com>
2026-07-02 17:21:44 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made By EBRP Development Team",
+        name: "Jerry T's diet",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 3,
       },
     ],
   },
